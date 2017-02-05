48cbf617dc22febf66d1525ccc925c6a04c503fd
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2017-02-05 18:26:01 -0300

    atualização

--- Projects/T2/main.cpp
+++ Projects/T2/main.cpp
@@ -1,14 +1,11 @@
 #include "Sorting_Algorithms.h"
 
 #include <stdlib.h>
-#include <dirent.h>
-#include <vector>
-#include <string>
-#include <fstream>
+#include <algorithm>
+#include <time.h>
 
 using namespace std;
 
-void searchDirFiles(const char* d, vector<string>* f);
 
 int main(int argc, char** argv)
 {
@@ -19,138 +16,92 @@ int main(int argc, char** argv)
 	int* input;
 	int* input_aux;
 	Sorting_Algorithms sorting;
-	vector<string> files;
-	ifstream f_in;
-	ofstream f_out;
+
+	clock_t c_t;
+	double dif;
 
 
-	if(argc > 1)
+	cin >> size;			
+	input = new int[size];
+	input_aux = new int[size];
+
+	while(cin >> in)
 	{
 
-		searchDirFiles(argv[1], &files);
-
-		for(int i = 0; i < files.size(); i++)
-		{
-
-			f_in.open(files[i].c_str());
-			if(f_in.is_open())
-			{
-
-				f_in >> size;
-				
-				input = new int[size];
-				input_aux = new int[size];
-
-				while(f_in >> in)
-				{
-
-					input[count] = in;
-					input_aux[count] = in;
-					count++;
-
-				}
-
-				//criar nome do arquivo de saida
-				//abrir aquivivo escrever algo no incio e começa as ordençaões
-
-				//ordenação
-
-				cout << "[1] Selection sort\n" << endl;
-				//tempo inicio
-				sorting.selectionSort(input, size);
-				//tempo fim
-				for(int i = 0; i < size; i++)
-	 				cout << input[i] << endl;
-
-				cout << "[2] Insertion sort\n" << endl;
-				sorting.copy(input, input_aux, size);
-				//tempo inicio
-				sorting.insertionSort(input, size);
-				//tempo fim
-				for(int i = 0; i < size; i++)
-	 				cout << input[i] << endl;
-
-				cout << "[3] Merge sort\n" << endl;
-				sorting.copy(input, input_aux, size);
-				//tempo inicio
-				sorting.mergeSort(input, 0, (size-1));
-				//tempo fim
-				for(int i = 0; i < size; i++)
-	 				cout << input[i] << endl;
-
-				cout << "[4] Quicksort\n" << endl;
-				sorting.copy(input, input_aux, size);
-				//tempo inicio
-				sorting.quickSort(input, 0, (size-1));
-				//tempo fim
-				for(int i = 0; i < size; i++)
-					cout << input[i] << endl;
-
-				cout << "[5] Heapsort\n" << endl;
-				sorting.copy(input, input_aux, size);
-				//tempo inicio
-				sorting.heapSort(input, (size));
-				//tempo fim
-				for(int i = 0; i < size; i++)
-					cout << input[i] << endl;
-
-			}
-			else
-			{
-
-				cout << "arquivo" << endl;
-
-			}
-
-		}
-
-	}
-	else
-	{
-
-		cout << "Argumento" << endl;
-
-	}
-	
-
-}
-
-void searchDirFiles(const char* d, vector<string>* f)
-{
-
-	DIR* dir = NULL;
-	struct dirent* drnt; 
-
-	dir = opendir(d);
-	if(dir)
-	{
-
-		while(drnt = readdir(dir))
-		{
-
-			int p = 0;
-			string s_aux = (string)drnt->d_name;
-			p = s_aux.find("~");
-
-			if(s_aux == "." || s_aux == ".." || s_aux[p] == '~'){cout << "if " << s_aux << endl;}
-            else
-            {
-cout << s_aux << endl;
-                f->push_back((string)drnt->d_name);
-
-            }
-
-		}
-
-	}
-	else
-	{
-
-		cout << "Diretorio nao encontrado, nao foi possivel abrir" << endl;
-		exit(1);
+		input[count] = in;
+		input_aux[count] = in;
+		count++;
 
 	}
 
-	closedir(dir);
+
+			
+	//ordenação
+	cout << "===================================================" << endl;
+	cout << "[1] Selection sort\n" << endl;
+	c_t = clock();
+	sorting.selectionSort(input, size);
+	c_t = clock() - c_t;
+	dif = ((double)c_t)/CLOCKS_PER_SEC;
+	cout << "Time " << dif << " s\n" << endl;
+	//for(int i = 0; i < size; i++)
+	//	cout << input[i] << endl;
+
+	cout << "\n\n\n===================================================" << endl;	
+	cout << "[2] Insertion sort\n" << endl;
+	sorting.copy(input, input_aux, size);
+	c_t = clock();
+	sorting.insertionSort(input, size);
+	c_t = clock() - c_t;
+	dif = ((double)c_t)/CLOCKS_PER_SEC;
+	cout << "Time " << dif << " s\n" << endl;
+	//for(int i = 0; i < size; i++)
+	//	cout << input[i] << endl;
+
+	cout << "\n\n\n===================================================" << endl;
+	cout << "[3] Merge sort\n" << endl;
+	sorting.copy(input, input_aux, size);
+	c_t = clock();
+	sorting.mergeSort(input, 0, (size-1));
+	c_t = clock() - c_t;
+	dif = ((double)c_t)/CLOCKS_PER_SEC;
+	cout << "Time " << dif << " s\n" << endl;
+	//for(int i = 0; i < size; i++)
+	//	cout << input[i] << endl;
+
+	cout << "\n\n\n===================================================" << endl;
+	cout << "[4] Quicksort\n" << endl;
+	sorting.copy(input, input_aux, size);
+	c_t = clock();
+	sorting.quickSort(input, 0, (size-1));
+	c_t = clock() - c_t;
+	dif = ((double)c_t)/CLOCKS_PER_SEC;
+	cout << "Time " << dif << " s\n" << endl;
+	//for(int i = 0; i < size; i++)
+	//	cout << input[i] << endl;
+
+	cout << "\n\n\n===================================================" << endl;
+	cout << "[5] Heapsort\n" << endl;
+	sorting.copy(input, input_aux, size);
+	c_t = clock();
+	sorting.heapSort(input, (size));
+	c_t = clock() - c_t;
+	dif = ((double)c_t)/CLOCKS_PER_SEC;
+	cout << "Time " << dif << " s\n" << endl;
+	//for(int i = 0; i < size; i++)
+	//	cout << input[i] << endl;
+
+	cout << "\n\n\n===================================================" << endl;
+	cout << "[5] Sort C++\n" << endl;
+	sorting.copy(input, input_aux, size);
+	c_t = clock();
+	sort(input, input+size);
+	c_t = clock() - c_t;
+	dif = ((double)c_t)/CLOCKS_PER_SEC;
+	cout << "Time " << dif << " s\n" << endl;
+	//for(int i = 0; i < size; i++)
+	//	cout << input[i] << endl;
+
+
 
 }
