3748d44c8795e4a4015b3a5c0ab4e5f637d2d753
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2017-03-22 09:23:21 -0300

    add all

---
+++ Projects/Mochila/README.md
@@ -0,0 +1,15 @@
+Trabalho 5 de APA - Problema da mochila
+===================
+
+Alunos: Felipe da Cunha, 11328535
+	Wellton Thyago de Souza, 11325715
+-----------
+
+**Atenção**, antes de executar o código, **leia** esse documento:
+
+> - Para compilar o código simplesmente faça: g++ -o out *.cpp.
+> - O projeto acompanha o makefile, é recomendado utilizá-lo.
+> - Após compilar o código, para executar basta seguir o exemplo: ./mochila < example.in > example.out
+
+
+
--- Projects/Mochila/problema_mochila.cpp
+++ Projects/Mochila/problema_mochila.cpp
@@ -1,6 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////
          /////////   Alunos: Felipe da Cunha Andrade Nunes, 11328535    /////////
 		/////////            Wellton Thyago de Souza, 11325715         /////////
+////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 #include <vector>
@@ -19,17 +20,21 @@ int main(int argc, char** argv)
 	int temp = 0;
 	vector<int> index;
 
+	//leituras das variáveis...
 	cin >> q_elem;
 	cin >> capacity;
 
+	// tabela de pesos e valores
 	mat_in = (int**)calloc(q_elem, sizeof(int*));
 	for(int i = 0; i < q_elem; i++)
 		mat_in[i] = (int*)calloc(2, sizeof(int));
 	
+	//tabela de elementos por capacidade
 	mat = (int**)calloc(q_elem+1, sizeof(int*));
 	for(int i = 0; i < q_elem+1; i++)
 		mat[i] = (int*)calloc(capacity+1, sizeof(int));
 
+	//preenchimento da matriz de pesos e valores
 	for(int i = 0; cin >> temp; i++)
 	{
 		if(i < q_elem)
@@ -38,13 +43,14 @@ int main(int argc, char** argv)
 			mat_in[i - q_elem][1] = temp;
 	}
 
+	//preenchendo a matriz de elementos e capacidade
 	for(int i = 1; i <= q_elem; i++)
 	{
 		for(int j = 1; j <= capacity; j++)
 		{
-			if(mat_in[i-1][0] <= j)
+			if(mat_in[i-1][0] <= j)// se o elemento cabe na mochila
 			{	
-				if((mat_in[i-1][1] + mat[i-1][j-mat_in[i-1][0]]) > mat[i-1][j])
+				if((mat_in[i-1][1] + mat[i-1][j-mat_in[i-1][0]]) > mat[i-1][j])//teste...
 					mat[i][j] = (mat_in[i-1][1] + mat[i-1][j-mat_in[i-1][0]]);
 				else
 					mat[i][j] = mat[i-1][j];
@@ -56,16 +62,15 @@ int main(int argc, char** argv)
 		}
 	}
 
+	//encontrando os indices que foram escolhidos
 	temp = capacity;
 	for(int i = q_elem; i > 0; i--)
 	{	
 		//cout << "i " << i << endl;
-		if((mat[i][temp] != mat[i-1][temp]) )
+		if((mat[i][temp] != mat[i-1][temp]) ) //se houver mudança...
 		{	
-			//cout << "oi" << endl;
 			index.push_back(i);
 			temp -= mat_in[i-1][0];
-			//cout << "temp " << temp << endl;
 		}
 		else
 		{
@@ -73,13 +78,15 @@ int main(int argc, char** argv)
 		}
 	}
 
+	//resposta final...
+	cout << "valor = " << mat[q_elem][capacity] << endl;
 	cout << "indices = " ;
 	for(int i = index.size()-1; i >= 0 ; i--)
 		cout << index[i] << " ";
-
 	cout << endl;
+	//responsta final...
 
-	cout << "valor = " << mat[q_elem][capacity] << endl;
+
 	#ifdef DEBUG
 	for(int i = 0; i < q_elem+1; i++)
 		for(int j = 0; j < capacity+1; j++)
